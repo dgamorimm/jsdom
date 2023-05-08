2fd49171bd774316accc15da2ba9a486602f743a
Douglas Amorim <douglas@gavea.com>
2023-05-08 14:57:36 -0300

    arithmetic assigments

---
+++ programming-logic/assignment-operators.js
@@ -0,0 +1,20 @@
+let n3 = 20;
+// n3 = n3 + 15;
+n3 += 15;
+console.log(n3);
+// n3 somado mais 15
+n3 -= 15;
+console.log(n3);
+// n3 subtraido menos 15
+n3 *= 15;
+console.log(n3);
+// n3 multiplicado 15
+n3 /= 15;
+console.log(n3);
+// n3 dividido 15
+n3 %= 15;
+console.log(n3);
+// n3 resto 15
+n3 **= 15;
+console.log(n3);
+// n3 potencia 15

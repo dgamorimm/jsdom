c9c020676c43bf59595c824f469c6cb757e772c3
Douglas Amorim <douglas@gavea.com>
2023-05-08 12:01:00 -0300

    primitive-data-types

---
+++ programming-logic/primitive-data-types.js
@@ -0,0 +1,11 @@
+// strings, number (int, floats), boolean
+
+let minhaVar = "minha string";
+let minhaVar2 = 'minha "string" cin aspas simples';
+var minhaVar3 = `minha template literal`;
+
+let idade = 28;
+// let msg = "eu possuo " + idade + " anos";
+let msg = `eu possuo ${idade} anos`;
+
+console.log(msg);

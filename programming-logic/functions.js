b64182ebcc52b7a9fa104888bcb9a3f02222a3e7
Douglas Amorim <douglas@gavea.com>
2023-05-08 17:28:40 -0300

    functions

---
+++ programming-logic/functions.js
@@ -0,0 +1,22 @@
+// declaração de função
+
+function hello() {
+    console.log('Hello World V1!');
+}
+
+hello();
+
+// expressão de função
+
+const hello2 = function() {
+    console.log('Hello World V2!');
+}
+
+hello2();
+
+// arrow functions
+const hello3 = () => {
+    console.log('Hello World V3!');
+}
+
+hello3();

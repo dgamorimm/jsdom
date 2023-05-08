4d3e1db5ca886949436eb2c325a217949ca2f964
Douglas Amorim <douglas@gavea.com>
2023-05-08 12:22:20 -0300

    number

---
+++ programming-logic/number.js
@@ -0,0 +1,4 @@
+const n1 = 10;
+const n2 = 1.1;
+console.log(`o tipo n1 é ${typeof n1} e seu valor é ${n1}`);
+console.log(`o tipo n2 é ${typeof n2} e seu valor é ${n2}`);
--- programming-logic/primitive-data-types.js
+++ programming-logic/primitive-data-types.js
@@ -5,7 +5,9 @@ let minhaVar2 = 'minha "string" cin aspas simples';
 var minhaVar3 = `minha template literal`;
 
 let idade = 28;
-// let msg = "eu possuo " + idade + " anos";
-let msg = `eu possuo ${idade} anos`;
+let msg1 = "eu possuo " + idade + " anos";
+let msg2 = `eu possuo ${idade} anos`;
 
-console.log(msg);
+console.log(msg1);
+console.log(msg2);
+console.log(typeof msg1, typeof idade, typeof minhaVar);

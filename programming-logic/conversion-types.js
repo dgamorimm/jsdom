3e1a47799cdf4e277edac1a1f35b849e41051440
Douglas Amorim <douglas@gavea.com>
2023-05-08 12:52:27 -0300

    conversion-types-2

--- programming-logic/conversion-types.js
+++ programming-logic/conversion-types.js
@@ -13,3 +13,12 @@ console.log(n1 + n2, typeof n1, typeof n2);
 // converte o numero para string e concatena
 // Coverter string em numeros (parseInt, parseFloat, Numeber())
 // parseInt e parseFloat (ignora as letras a direita caso haja ex.: 10sdfgskgbhkfdbfdh)
+
+let n3 = 10;
+let n4 = 2;
+n4 = n4.toString(10); //base 10, ja esta implicto e não precisa passar
+console.log(n4, typeof n4);
+let n5 = 15;
+n5 = n5.toString(16); //base hexadecimal
+console.log(n5, typeof n5);
+// convertendo para string

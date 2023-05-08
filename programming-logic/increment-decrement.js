baad0fc3abb804a0259b681246be076601bcf987
Douglas Amorim <douglas@gavea.com>
2023-05-08 16:09:42 -0300

    incrment-decrement

---
+++ programming-logic/increment-decrement.js
@@ -0,0 +1,14 @@
+//pós-incremento i++;
+//pós-decremento i--;
+//pré-incremento ++i;
+//pré-decremento --i;
+
+let i = 0;
+console.log(i++);
+console.log(i);
+//pós-incremento i++;
+
+let j = 0;
+console.log(++j);
+console.log(j);
+//pré-incremento ++i;

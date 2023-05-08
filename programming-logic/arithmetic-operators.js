fd5d6300b59e7c1e482f2a44e5369d608cbb001c
Douglas Amorim <douglas@gavea.com>
2023-05-08 12:57:04 -0300

    arithmetic operators

---
+++ programming-logic/arithmetic-operators.js
@@ -0,0 +1,13 @@
+// + - * /
+// % = Resto da divisão
+// ** = Potencia
+
+let n1 = 10;
+let n2 = 5;
+
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 * n2);
+console.log(n1 / n2);
+console.log(n1 % n2);
+console.log(2 ** 3);

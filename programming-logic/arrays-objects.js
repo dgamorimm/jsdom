6214ec007fb13f2800178bb89a77c83e5beb4386
Douglas Amorim <douglas@gavea.com>
2023-05-08 19:14:04 -0300

    array-objects

---
+++ programming-logic/arrays-objects.js
@@ -0,0 +1,10 @@
+// const nomes = ['' , '', ''];
+const pessoa = {nome: 'Douglas', idade : 10, email: 'douglas@server.com'};
+const pessoa1 = {nome: 'Maria', idade : 40, email: 'maria@server.com'};
+const pessoa2 = {nome: 'Joao', idade : 35, email: 'joao@server.com'};
+// e seu eu tiver mais pessoas ?
+
+const pessoas = [pessoa, pessoa1, pessoa2];
+
+console.log(pessoas);
+console.log(pessoas[0].nome);

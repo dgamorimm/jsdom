1a5d36f2c348ba13857378eb55d833f02c22cb46
Douglas Amorim <douglas@gavea.com>
2023-05-08 16:38:10 -0300

    falsy-truthy

---
+++ programming-logic/ternary-operator.js
@@ -0,0 +1,12 @@
+let idade = 16;
+
+// let msgMaiorIdade = '';
+// if (idade >= 18) {
+//     msgMaiorIdade = 'É maior de idade'
+// }else{
+//     msgMaiorIdade = 'É menor de idade'
+// }
+
+let msgMaiorIdade = (idade >= 18) ? 'É maior de idade' : 'É menor de idade';
+
+console.log(msgMaiorIdade);
